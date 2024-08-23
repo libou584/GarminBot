8aa79957b2d22da4d4b9d4c04490addfd5dc9d5d
lilian <li4bou@gmail.com>
2024-08-23 10:42:17 +0200

    added stress data and minutes of activity to prompt

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import os
 
 
 def addSleepData(client, date) :
-    promptpart = "Sleep data :\n"
+    promptpart = "Sleep data (sleep time and resting heart rate) :\n"
     for i in range(1, 8) :
         sleep = client.get_sleep_data((date - datetime.timedelta(days=i)).strftime("%Y-%m-%d"))
-        promptpart += "On day " + str(i) + ", sleep time : " + str(round(sleep["dailySleepDTO"]["sleepTimeSeconds"]/3600, 1)) + " hours, resting heart rate : " + str(sleep["restingHeartRate"]) + "\n"
+        promptpart += "On day " + str(i) + " : " + str(round(sleep["dailySleepDTO"]["sleepTimeSeconds"]/3600, 1)) + " hours, " + str(sleep["restingHeartRate"]) + " bpm\n"
     return promptpart + "\n"
 
 
@@ -16,8 +16,24 @@ def addDailyStepsData(client, date) :
     promptpart = "Daily steps data :\n"
     steps = client.get_daily_steps((date - datetime.timedelta(days=7)).strftime("%Y-%m-%d"), (date - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
     for i in range(7) :
-        promptpart += "On day " + str(i+1) + ", steps : " + str(steps[i]["totalSteps"]) + "\n"
+        promptpart += "On day " + str(i+1) + " : " + str(steps[i]["totalSteps"]) + "\n"
     return promptpart + "\n"
+
+
+def addStressData(client, date) :
+    promptpart = "Average stress levels (out of 100) :\n"
+    for i in range(1, 8) :
+        stress = client.get_stress_data((date - datetime.timedelta(days=i)).strftime("%Y-%m-%d"))
+        promptpart += "On day " + str(i) + " : " + str(stress["avgStressLevel"]) + "\n"
+    return promptpart + "\n"
+
+
+def addActivityMinutesData(client, date) :
+    activities = client.get_activities_by_date((date - datetime.timedelta(days=7)).strftime("%Y-%m-%d"), (date - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
+    totalMinutes = 0
+    for activity in activities :
+        totalMinutes += int(activity["duration"]/60)
+    return "Activity data : " + str(totalMinutes) + " minutes total\n\n"
 
 
 def main() :
@@ -31,13 +47,10 @@ def main() :
     prompt = "You are a personal health assistant analyzing my weekly health data. Your task is to provide insights, and offer recommendations based on the following data of the last 7 days :\n\n"
     prompt += addSleepData(client, date)
     prompt += addDailyStepsData(client, date)
+    prompt += addStressData(client, date)
+    prompt += addActivityMinutesData(client, date)
 
     print(prompt)
-    # activity = client.get_stress_data("2024-08-12")["avgStressLevel"]
-    # activity = client.get_activities_by_date("2024-08-19", "2024-08-22")
-    # for a in activity:
-    #     print(a["activityType"]["typeKey"])
-    # print(activity)
 
 
 if __name__ == "__main__" :
